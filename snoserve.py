17a5188c21821fbd28e60769eb5b5d27f5991581
seesteins <chris.steinley@gmail.com>
2024-06-22 19:25:00 -0400

    added documentation, handled workspace issue

--- snoserve.py
+++ snoserve.py
@@ -12,8 +12,34 @@ from osgeo.gdal import Translate, TranslateOptions
 from pytz import timezone
 
 
-class dataDate:  # a class to get the time for data download and naming purposes
+class dataDate:
+    """
+    A class to determine the appropriate date for data download and naming purposes.
+
+    This class determines the latest date for which SNODAS data should be downloaded and processed.
+    It accounts for the release time of the data, which is typically at 9:15 AM Eastern Time.
+    If the current time is before the release time, the class will use the date from the previous day.
+
+    Attributes:
+        latest_data (datetime.datetime): The latest date for which SNODAS data should be downloaded and processed.
+        year (str): The year component of latest_data in YYYY format.
+        day (str): The day component of latest_data in DD format.
+        month (str): The month component of latest_data in MM format.
+        date_string (str): The date string in YYYYMMDD format.
+        monthAbbrv (str): The abbreviated month name (e.g., Jan, Feb, Mar) corresponding to latest_data.
+
+    Example:
+        >>> date_obj = dataDate()
+        >>> print(date_obj.latest_data)
+        2023-06-10 00:00:00-04:00
+        >>> print(date_obj.date_string)
+        20230610
+    """
+
     def __init__(self):
+        """
+        Initializes the dataDate object and determines the latest date for SNODAS data download and processing.
+        """
         tz = timezone("US/Eastern")
         now = datetime.now(tz)
         releaseTime = now.replace(hour=9, minute=15, second=0, microsecond=0)
@@ -28,25 +54,75 @@ class dataDate:  # a class to get the time for data download and naming purposes
         self.monthAbbrv = self.latest_data.strftime("%b")
 
 
-class file:  # download data and drives the extraction and processing
+class file:
+    """
+    A class for downloading and processing SNODAS data.
+
+    This class handles the downloading, extraction, and processing of SNODAS data for a given date.
+    It creates the necessary directories, downloads the data, extracts the compressed files,
+    and prepares the data for further processing.
+
+    Attributes:
+        date (dataDate): A dataDate object containing the date information for the data download.
+        dir (directory): A directory object containing the paths for various directories used in the processing.
+        address (str): The URL address for downloading the SNODAS data.
+
+    Methods:
+        download(): Downloads the SNODAS data if it hasn't been downloaded already.
+        extractTAR(): Extracts the downloaded TAR file.
+        extractGZ(): Extracts all GZ files from the extracted TAR file.
+    """
     def __init__(self, date, directory):
+        """
+        Initializes the file object with the given date and directory information.
+
+        Args:
+            date (dataDate): A dataDate object containing the date information for the data download.
+            directory (directory): A directory object containing the paths for various directories used in the processing.
+        """
         self.date = date
         self.dir = directory
         self.dir.create()
         self.address = f"https://noaadata.apps.nsidc.org/NOAA/G02158/unmasked/{self.date.year}/{self.date.month}_{self.date.monthAbbrv}/SNODAS_unmasked_{self.date.year}{self.date.month}{self.date.day}.tar"
 
-    def download(self):  # downloads current SNODAS data if not existing
-        if not isfile(self.dir.download):  # might remove for production
+    def download(self):
+        """
+        Downloads the SNODAS data if it hasn't been downloaded already.
+
+        This method checks if the SNODAS data file has already been downloaded.
+        If the file doesn't exist, it downloads the data from the specified URL address.
+        If the file already exists, it prints a message indicating that the file has been downloaded.
+        """
+        if not isfile(self.dir.download):
+            # Set the no_proxy environment variable to allow downloading without a proxy
             environ["no_proxy"] = "*"
             print(urlretrieve(self.address, self.dir.download))
         else:
             print("file already downloaded; proceeding")
 
-    def extractTAR(self):  # extract download tar file
+    def extractTAR(self):
+        """
+        Extracts the downloaded TAR file.
+
+        This method extracts the downloaded TAR file into the specified extraction directory.
+        The extracted files are used for further processing.
+
+        Returns:
+            str: The path to the directory where the TAR file was extracted.
+        """
         unpack_archive(self.dir.download, self.dir.extract)
         return self.dir.extract
 
-    def extractGZ(self):  # extract all .gz files from .tar extraction rm originals
+    def extractGZ(self):
+        """
+        Extracts all GZ files from the extracted TAR file and removes the original GZ files.
+
+        This method navigates to the directory where the TAR file was extracted.
+        It then iterates through all files in the directory and checks for files with the '.gz' extension.
+        For each GZ file found, it extracts the contents and creates a new file without the '.gz' extension.
+        After extracting the contents, the original GZ file is removed.
+        Finally, the method changes the current working directory back to the original directory.
+        """
         extension = ".gz"
         chdir(self.dir.extract)
         for item in listdir(self.dir.extract):
@@ -103,15 +179,31 @@ class file:  # download data and drives the extraction and processing
 
 class GTIFF:  # processes individual geotiff files
     def __init__(self, filename, directory):
-        # pass in a filename and directory object
+        """
+        Initializes the GTIFF class for processing individual GeoTIFF files.
+
+        Args:
+            filename (str): The base filename of the GeoTIFF files (without extensions).
+            directory (object): A directory object containing the necessary paths.
+
+        Attributes:
+            txt (str): The file path for the associated .txt file.
+            dat (str): The file path for the associated .dat file.
+            hdr (str): The file path for the associated .hdr file.
+            metadata (dict): A dictionary containing the metadata read from the .txt file.
+        """
         self.txt = join(directory.extract, f"{filename}.txt")  # set .txt file path
         self.dat = join(directory.extract, f"{filename}.dat")  # set .dat file path
         self.hdr = join(directory.extract, f"{filename}.hdr")
         self.metadata = read_txt_vars(self.txt)
 
     def stringHDR(self):
-        # creates the text of the .hdr file - might be able to use this without an
-        # an additional file operation
+        """
+        Generates the text content for the ENVI header file (.hdr) based on the metadata.
+
+        Returns:
+            str: The text content for the ENVI header file.
+        """
         samples = self.metadata["Number of columns"]
         lines = self.metadata["Number of rows"]
         self.envi = [
@@ -129,12 +221,25 @@ class GTIFF:  # processes individual geotiff files
         return self.envi
 
     def createHDR(self):
+        """
+        Creates the ENVI header file (.hdr) with the content generated from the stringHDR method.
+
+        Returns:
+            str: The file path of the created .hdr file.
+        """
         self.stringHDR()
         with open(self.hdr, "w") as hdr:
             hdr.write(self.envi)
         return self.hdr
 
     def process(self, dir, filename):
+        """
+        Processes the .dat file and generates a GeoTIFF file with the specified filename.
+
+        Args:
+            dir (object): A directory object containing the necessary paths.
+            filename (str): The desired filename for the output GeoTIFF file.
+        """
         minX = float(self.metadata["Minimum x-axis coordinate"])
         minY = float(self.metadata["Minimum y-axis coordinate"])
         maxX = float(self.metadata["Maximum x-axis coordinate"])
@@ -154,7 +259,20 @@ class GTIFF:  # processes individual geotiff files
         self.name = filename
 
     def colorize(self, dir, colortxt=None, output_file=None):
-        # need to call a command in the shell for this to work
+        """
+        Applies color relief to the GeoTIFF file using a color table file.
+
+        Args:
+            dir (object): A directory object containing the necessary paths.
+            colortxt (str, optional): The file path of the color table file. If not provided,
+                the default file path is constructed based on the GTIFF name.
+            output_file (str, optional): The file path for the output GeoTIFF file with color relief.
+                If not provided, the original GeoTIFF file will be overwritten.
+
+        This method calls the 'gdaldem color-relief' command in the shell to apply color relief
+        to the GeoTIFF file using the specified color table file. The '-alpha' option is used to
+        preserve the alpha channel (if present) in the output file.
+        """
         if output_file is None:
             output_file = self.fullPath
         if colortxt is None:
@@ -163,11 +281,20 @@ class GTIFF:  # processes individual geotiff files
         check_call(cmd, shell=True)
 
     def convertToInches(self):
+        """
+        This method is a placeholder for future work related to converting the SNODAS snow depth data
+        to inches.
+        """
         pass  # future work
-
 
 class directory:  # directory manager
     def __init__(self, date):
+        """
+        Initializes the directory object with paths and filenames based on the provided date.
+
+        Args:
+            date (dataDate): A dataDate object containing the current date information.
+        """
         self.workingDirectory = dirname(abspath(__file__))
         self.date = f"{date.year}{date.month}{date.day}"
         self.name = f"SNODAS-{self.date}"
@@ -178,7 +305,6 @@ class directory:  # directory manager
         self.finalData = join(self.data, self.name)
         self.swe = join(self.finalData, f"swe{self.date}.tif")
         self.snowDepth = join(self.finalData, f"snowdepth{self.date}.tif")
-        # self.colortables = join(self.workingDirectory, "colortables")
         self.styles = join(self.workingDirectory, "styles")
         self.folders = [
             self.data,
@@ -192,13 +318,19 @@ class directory:  # directory manager
         self.environment = join(self.workingDirectory, ".env")
 
     def create(self):
-        # creates folders for data
+        """
+        Creates directories for storing data, temporary files, and styles.
+        """
         for folder in self.folders:
             Path(folder).mkdir(parents=True, exist_ok=True)
 
     def outputDirs(self):
-        # takes the filenames.txt file and creates a dictionary with the locations to
-        # to save processed data
+        """
+        Reads the file names from the filenames.txt file and creates a dictionary with the locations to save processed data.
+
+        Returns:
+            dict: A dictionary where keys are file descriptions and values are output file paths.
+        """
         self.finalNames = read_txt_vars(self.filenames)
         self.outputPaths = {}
         for key in self.finalNames:
@@ -206,11 +338,21 @@ class directory:  # directory manager
         return self.outputPaths
 
     def unzippedName(self, extension, zippedFile):  # refactor extract GZ in future
+        """
+        This method is a placeholder for future refactoring related to extracting GZ files.
+        """
         pass
 
 
 class server:
     def __init__(self, directory):
+        """
+        Initialize a server instance with the given directory.
+
+        Args:
+            directory (object): A directory object containing the necessary information
+                for the server, such as the styles folder path.
+        """
         self.directory = directory
         self.HOST =getenv("GEOSERVER_ADDRESS")
         self.USERNAME = getenv("GEOSERVER_USERNAME")
@@ -219,6 +361,17 @@ class server:
         self.geoserver
 
     def upload_data(self, data_name, workspace, local_path):
+        """
+        Upload geospatial data to the GeoServer instance.
+
+        Args:
+            data_name (str): The name of the data to be uploaded.
+            workspace (str): The workspace in GeoServer where the data will be uploaded.
+            local_path (str): The local file path of the data to be uploaded.
+
+        Returns:
+            A CoverageStore object representing the uploaded data.
+        """
         return self.geoserver.create_coveragestore(
             data_name,
             workspace=workspace,
@@ -240,6 +393,9 @@ class server:
         Finally, it sends a PUT request to the GeoServer REST API using cURL to set the
         default style for the layer.
 
+        As far as I can tell there is no way to set a default style for a layer using the
+        geserver-restconfig module.
+
         Args:
             layer_name (str): The name of the layer to be styled.
             style_name (str): The name of the style to be applied to the layer.
@@ -254,6 +410,13 @@ class server:
         system(cmd)
 
     def upload_folder(self, workspace, folder_path):
+        """
+        Upload all GeoTIFF files from a specified folder to the GeoServer instance.
+
+        Args:
+            workspace (str): The workspace in GeoServer where the data will be uploaded.
+            folder_path (str): The local path of the folder containing the GeoTIFF files.
+        """
         for data in listdir(folder_path):
             if data.endswith(".tif"):
                 data_path = join(folder_path, data)
@@ -261,6 +424,16 @@ class server:
                 self.upload_data(name, workspace, data_path)
 
     def delete_data(self, data_name, workspace):
+        """
+        Delete geospatial data from the GeoServer instance.
+
+        Args:
+            data_name (str): The name of the data to be deleted.
+            workspace (str): The workspace in GeoServer where the data is located.
+
+        Returns:
+            A boolean indicating whether the deletion was successful.
+        """
         store = self.geoserver.get_store(name=data_name, workspace=workspace)
         return self.geoserver.delete(store, purge=True, recurse=True)
 
@@ -293,6 +466,12 @@ class server:
             raise Exception(f"Style {style} not found in styles folder.")
 
     def style_types(self, types_list):
+        """
+        Style all data stores in GeoServer that have a name matching the provided list of types.
+
+        Args:
+            types_list (list): A list of data types (store names) to be styled.
+        """
         stores = self.geoserver.get_stores()
         for store in stores:
             data_type = store.name
@@ -300,15 +479,49 @@ class server:
                 self.style_data(store.name, data_type)
 
     def selective_upload(self, workspace, folder_path, selection):
+        """
+        Upload selected GeoTIFF files from a specified folder to the GeoServer instance.
+
+        Args:
+            workspace (str): The workspace in GeoServer where the data will be uploaded.
+            folder_path (str): The local path of the folder containing the GeoTIFF files.
+            selection (list): A list of file names (without extensions) to be uploaded.
+        """
         selection = [f"{file}.tif" for file in selection]
         for file in listdir(folder_path):
             if file in selection:
                 data_path = join(folder_path, file)
                 name = (path.basename(file)).rsplit(".", 1)[0]
                 self.upload_data(name, workspace, data_path)
+    
+    def check_workspace(self, workspace):
+        """
+        Check if a workspace exists in the GeoServer instance.
+        If the workspace does not exist, create it.
+
+        Args:
+            workspace (str): The name of the workspace to check/create.
+
+        Returns:
+            None
+        """
+        workspaces = self.geoserver.get_workspaces()
+        if workspace not in workspaces:
+            self.geoserver.create_workspace(workspace, workspace)
+        else:
+            print(f"Workspace '{workspace}' already exists.")
 
 
-def read_txt_vars(txt):  # reads .txt into a dictionary "key: value\n"
+def read_txt_vars(txt):
+    """
+    Read key-value pairs from a text file and store them in a dictionary.
+
+    Args:
+        txt (str): Path to the text file.
+
+    Returns:
+        dict: A dictionary containing the key-value pairs read from the file.
+    """
     variables = {}
     with open(txt) as varfile:
         for var in varfile:
@@ -317,11 +530,29 @@ def read_txt_vars(txt):  # reads .txt into a dictionary "key: value\n"
     return variables
 
 
-def strip_extension(file):  # strips the first extension from a file
+def strip_extension(file):
+    """
+    Remove the first file extension from a file path or name.
+
+    Args:
+        file (str): The file path or name.
+
+    Returns:
+        str: The file name without the first extension.
+    """
     return (path.basename(file)).rsplit(".", 1)[0]
 
 
 def datetime_from_str(string):
+    """
+    Convert a string in the format 'YYYYMMDD' to a datetime object in the US/Eastern timezone.
+
+    Args:
+        string (str): A string representing a date in the format 'YYYYMMDD'.
+
+    Returns:
+        datetime.datetime: A datetime object representing the input date in the US/Eastern timezone.
+    """
     tz = timezone("US/Eastern")
     date = datetime.strptime(string, "%Y%m%d").replace(tzinfo=tz)
     return date
